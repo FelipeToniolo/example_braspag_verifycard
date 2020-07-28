a8d558f3951406f14ec42043f4ddb0cdec75a9e2
ftoniolo <ftoniolo@braspag.com.br>
2020-07-28 11:25:40 -0300

    titulo do app

--- lib/main.dart
+++ lib/main.dart
@@ -59,7 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Demo Braspag Silent Order Post'),
+        title: Text('Demo Braspag Verify Card'),
         centerTitle: true,
       ),
       body: DefaultTextStyle(
